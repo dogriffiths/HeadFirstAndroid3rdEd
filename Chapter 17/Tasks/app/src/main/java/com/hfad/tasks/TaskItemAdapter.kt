2cfcee4acdb260aa596eed0f4486bacdd0b3ca9c
davidg <dogriffiths@gmail.com>
2022-01-14 15:20:31 +0000

    chore: Add code for chapter 17

---
+++ Chapter 17/Tasks/app/src/main/java/com/hfad/tasks/TaskItemAdapter.kt
@@ -0,0 +1,36 @@
+package com.hfad.tasks
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.hfad.tasks.databinding.TaskItemBinding
+
+class TaskItemAdapter(val clickListener: (taskId: Long) -> Unit)
+    : ListAdapter<Task, TaskItemAdapter.TaskItemViewHolder>(TaskDiffItemCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int)
+            : TaskItemViewHolder = TaskItemViewHolder.inflateFrom(parent)
+
+    override fun onBindViewHolder(holder: TaskItemViewHolder, position: Int) {
+        val item = getItem(position)
+        holder.bind(item, clickListener)
+    }
+
+    class TaskItemViewHolder(val binding: TaskItemBinding)
+        : RecyclerView.ViewHolder(binding.root) {
+
+        companion object {
+            fun inflateFrom(parent: ViewGroup): TaskItemViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = TaskItemBinding.inflate(layoutInflater, parent, false)
+                return TaskItemViewHolder(binding)
+            }
+        }
+
+        fun bind(item: Task, clickListener: (taskId: Long) -> Unit) {
+            binding.task = item
+            binding.root.setOnClickListener { clickListener(item.taskId) }
+        }
+    }
+}
---
+++ Chapter 17/Tasks/app/src/main/java/com/hfad/tasks/TasksFragment.kt
@@ -0,0 +1,59 @@
+package com.hfad.tasks
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.hfad.tasks.databinding.FragmentTasksBinding
+
+class TasksFragment : Fragment() {
+    private var _binding: FragmentTasksBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentTasksBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        val application = requireNotNull(this.activity).application
+        val dao = TaskDatabase.getInstance(application).taskDao
+
+        val viewModelFactory = TasksViewModelFactory(dao)
+        val viewModel = ViewModelProvider(
+            this, viewModelFactory).get(TasksViewModel::class.java)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        val adapter = TaskItemAdapter { taskId ->
+            viewModel.onTaskClicked(taskId)
+        }
+        binding.tasksList.adapter = adapter
+
+        viewModel.tasks.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                adapter.submitList(it)
+            }
+        })
+
+        viewModel.navigateToTask.observe(viewLifecycleOwner, Observer { taskId ->
+            taskId?.let {
+                val action = TasksFragmentDirections
+                    .actionTasksFragmentToEditTaskFragment(taskId)
+                this.findNavController().navigate(action)
+                viewModel.onTaskNavigated()
+            }
+        })
+
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
