620c86b9f561f8de6ba19e15ab562ee224c9d5ad
davidg <dogriffiths@gmail.com>
2022-01-14 13:51:55 +0000

    chore: Add guessing game to chapter 11

---
+++ Chapter 11/GuessingGame/app/src/main/java/com/hfad/guessinggame/GameFragment.kt
@@ -0,0 +1,49 @@
+package com.hfad.guessinggame
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.hfad.guessinggame.databinding.FragmentGameBinding
+import androidx.navigation.findNavController
+import androidx.lifecycle.ViewModelProvider
+
+class GameFragment : Fragment() {
+    private var _binding: FragmentGameBinding? = null
+    private val binding get() = _binding!!
+    lateinit var viewModel: GameViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentGameBinding.inflate(inflater, container, false)
+        val view = binding.root
+        viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
+
+        updateScreen()
+
+        binding.guessButton.setOnClickListener() {
+            viewModel.makeGuess(binding.guess.text.toString().uppercase())
+            binding.guess.text = null
+            updateScreen()
+            if (viewModel.isWon() || viewModel.isLost()) {
+                val action = GameFragmentDirections
+                    .actionGameFragmentToResultFragment(viewModel.wonLostMessage())
+                view.findNavController().navigate(action)
+            }
+        }
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    fun updateScreen() {
+        binding.word.text = viewModel.secretWordDisplay
+        binding.lives.text = "You have ${viewModel.livesLeft} lives left."
+        binding.incorrectGuesses.text = "Incorrect guesses: ${viewModel.incorrectGuesses}"
+    }
+}
