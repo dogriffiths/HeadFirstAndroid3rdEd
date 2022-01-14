2762de1df7dc00b61b696873d073297654f512fb
davidg <dogriffiths@gmail.com>
2022-01-14 10:10:31 +0000

    chore: Add app for chapter 7

--- Chapter 07/SecretMessage/app/src/main/java/com/hfad/secretmessage/MessageFragment.kt
+++ Chapter 07/SecretMessage/app/src/main/java/com/hfad/secretmessage/MessageFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.navigation.findNavController
 
 class MessageFragment : Fragment() {
     override fun onCreateView(
@@ -12,7 +15,16 @@ class MessageFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        //Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_message, container, false)
+        val view = inflater.inflate(R.layout.fragment_message, container, false)
+        val nextButton = view.findViewById<Button>(R.id.next)
+        val messageView = view.findViewById<EditText>(R.id.message)
+
+        nextButton.setOnClickListener {
+            val message = messageView.text.toString()
+            val action = MessageFragmentDirections
+                .actionMessageFragmentToEncryptFragment(message)
+            view.findNavController().navigate(action)
+        }
+        return view
     }
 }
